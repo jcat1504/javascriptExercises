8eb74d2d404e494d90e892117c81771e50c7a12d
Jess Y <31145424+jcat1504@users.noreply.github.com>
2019-06-27 19:22:57 -0700

    Add files via upload

---
+++ simpleArraySum.js
@@ -0,0 +1,8 @@
+//Given an array of integers, find the sum of its elements.
+
+function simpleArraySum(ar) {
+    return ar.reduce((a, b) => a + b)
+  //.reduce reduces array to single value
+}
+
+//when is this good for? figuring out the sum of array elements all added together.
