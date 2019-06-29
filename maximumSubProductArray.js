23a01706491249879c23a36ecb5dc82562e4d4e4
Jess Y <31145424+jcat1504@users.noreply.github.com>
2019-06-28 17:59:36 -0700

    Add files via upload

---
+++ maximumSubProductArray.js
@@ -0,0 +1,14 @@
+//Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  let partialSum = 0;
+
+  for (let item of arr) { // for each item of arr
+    partialSum += item; // add it to partialSum
+    maxSum = Math.max(maxSum, partialSum); // remember the maximum
+    if (partialSum < 0) partialSum = 0; // zero if negative
+  }
+
+  return maxSum;
+}
